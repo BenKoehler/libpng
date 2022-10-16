ac7a0b7036741e3857757b75edd3b98e4db073ec
BenKoehler <ben.koehler@web.de>
2022-10-16 10:47:47 +0200

    replace CMAKE_CURRENT_SOURCE_DIR with PROJECT_SOURCE_DIR

--- bk.cmake
+++ bk.cmake
@@ -1,4 +1,4 @@
 function(link_libpng target MOD)
-    target_link_libraries(${target} ${MOD} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/libpng/lib/libpng.so)
-    target_include_directories(${target} SYSTEM ${MOD} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/libpng/include)
+    target_link_libraries(${target} ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/libpng/lib/libpng.so)
+    target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/libpng/include)
 endfunction()
