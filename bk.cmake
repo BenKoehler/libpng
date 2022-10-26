273e89b9a0e7bbd8c1ba092aa3412be7c9d3fa64
BenKoehler <ben.koehler@web.de>
2022-10-26 14:56:41 +0200

    add version macro

--- bk.cmake
+++ bk.cmake
@@ -1,3 +1,4 @@
+set(BK_LIBPNG_VERSION "16.38.git")
 function(link_libpng target MOD)
     target_link_libraries(${target} ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/libpng/lib/libpng.so)
     target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/libpng/include)
